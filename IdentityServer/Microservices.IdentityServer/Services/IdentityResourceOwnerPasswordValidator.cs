5fc229b7ca0fe615481e8fb9cf8ab25b7a5d7e1d
SezginKahraman <sezgin.kahraman@gmail.com>
2023-05-07 21:09:49 +0300

    Custom validation is added.

---
+++ IdentityServer/Microservices.IdentityServer/Services/IdentityResourceOwnerPasswordValidator.cs
@@ -0,0 +1,45 @@
+﻿using IdentityModel;
+using IdentityServer4.Validation;
+using Microservices.IdentityServer.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Microservices.IdentityServer.Services
+{
+    public class IdentityResourceOwnerPasswordValidator : IResourceOwnerPasswordValidator
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public IdentityResourceOwnerPasswordValidator(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public async Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
+        {
+            var existUser = await userManager.FindByEmailAsync(context.UserName);
+
+            if (existUser == null)
+            {
+                var errors = new Dictionary<string, object>();
+
+                errors.Add("errors", new List<string>() { "email veya şifreniz yanlış." });
+
+                context.Result.CustomResponse = errors;
+
+                return;
+            }
+            var passwordCheck = await userManager.CheckPasswordAsync(existUser, context.Password);
+
+            if (!passwordCheck)
+            {
+                var errors = new Dictionary<string, object>();
+                errors.Add("errors", new List<string>() { "email veya şifre yanlış."});
+                context.Result.CustomResponse = errors;
+                return;
+            }
+            context.Result = new GrantValidationResult(existUser.Id.ToString(), OidcConstants.AuthenticationMethods.Password);
+        }
+    }
+}
--- IdentityServer/Microservices.IdentityServer/Startup.cs
+++ IdentityServer/Microservices.IdentityServer/Startup.cs
@@ -5,6 +5,7 @@
 using IdentityServer4;
 using Microservices.IdentityServer.Data;
 using Microservices.IdentityServer.Models;
+using Microservices.IdentityServer.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -54,6 +55,8 @@ namespace Microservices.IdentityServer
                 .AddInMemoryApiScopes(Config.ApiScopes)
                 .AddInMemoryClients(Config.Clients)
                 .AddAspNetIdentity<ApplicationUser>();
+
+            builder.AddResourceOwnerValidator<IdentityResourceOwnerPasswordValidator>();
 
             // not recommended for production - you need to store your key material somewhere secure
             builder.AddDeveloperSigningCredential();
