ca1958dbe62799d2065a410a559b88a417f9ba65
SezginKahraman <sezgin.kahraman@gmail.com>
2023-05-07 21:41:59 +0300

    GetUser is added.

--- IdentityServer/Microservices.IdentityServer/Controllers/UserController.cs
+++ IdentityServer/Microservices.IdentityServer/Controllers/UserController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.JsonWebTokens;
 using System.Linq;
 using System.Threading.Tasks;
 using static IdentityServer4.IdentityServerConstants;
@@ -40,6 +41,20 @@ namespace Microservices.IdentityServer.Controllers
             }
 
             return NoContent();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUser()
+        {
+            var userIdClaim = User.Claims.FirstOrDefault(x=> x.Type == JwtRegisteredClaimNames.Sub);
+
+            if (userIdClaim == null) return BadRequest();
+
+            var user = await _userManager.FindByIdAsync(userIdClaim.Value);
+
+            if (user == null) return BadRequest();
+
+            return Ok(new {Id = user.Id, UserName = user.UserName, City = user.City});
         }
     }
 }
