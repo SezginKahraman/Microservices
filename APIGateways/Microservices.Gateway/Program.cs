dbd2fff864b46601fe2dcb445eab0f65bc7f9837
SezginKahraman <sezgin.kahraman@gmail.com>
2023-09-02 18:17:03 +0300

    Ocelot library configurations are added.

--- APIGateways/Microservices.Gateway/Program.cs
+++ APIGateways/Microservices.Gateway/Program.cs
@@ -1,6 +1,19 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOcelot();
+
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config.AddJsonFile($"configuration.{hostingContext.HostingEnvironment.EnvironmentName.ToLower()}.json").AddEnvironmentVariables();
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+await app.UseOcelot();
 
 app.Run();
