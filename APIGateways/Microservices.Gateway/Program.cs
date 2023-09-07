3dee62e8f927623050845219d6ffff4b58ffdff4
SezginKahraman <sezgin.kahraman@gmail.com>
2023-09-07 22:38:56 +0300

    Identity set up for gateway is done.

--- APIGateways/Microservices.Gateway/Program.cs
+++ APIGateways/Microservices.Gateway/Program.cs
@@ -1,7 +1,26 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using System.IdentityModel.Tokens.Jwt;
 
 var builder = WebApplication.CreateBuilder(args);
+
+#region [ Configuring application ]
+
+#region [ Make secure the gateway by adding jwt ]
+
+builder.Services.AddAuthentication().AddJwtBearer("GatewayAuthenticationSchema", o =>
+{
+    o.Authority = builder.Configuration["IdentityServerURL"];
+    o.Audience = "resource_gateway";
+    o.RequireHttpsMetadata = false;
+});
+
+#endregion
+
+#endregion [ Configuring application ]
 
 builder.Services.AddOcelot();
 
@@ -13,6 +32,12 @@ builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+#region [ Add authentication ]
+
+app.UseAuthentication();
+
+#endregion [ Authentication ]
 
 await app.UseOcelot();
 
